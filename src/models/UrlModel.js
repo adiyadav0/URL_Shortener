c6eb913084998c07282da15e246916e87cbc5ebe
adiyadav0 <adiyadav596@gmail.com>
2022-07-18 11:56:07 +0530

    url index

---
+++ src/models/UrlModel.js
@@ -0,0 +1,25 @@
+const mongoose =require('mongoose');
+const urlSchema = new mongoose.Schema({
+    urlCode: { 
+        type:String, 
+        unique:true,
+        required:true,
+        trim:true
+         }, 
+
+    longUrl: {
+        type:String,
+        required:true,
+        trim:true 
+    },
+
+    shortUrl: {
+        type:String,
+        required:true,
+         unique:true,
+         trim:true
+        }
+
+},{timetamps:true});
+
+module.exports= mongoose.model('urlShortener',urlSchema)
---
+++ src/route/route.js
@@ -0,0 +1,8 @@
+const express= require('express');
+const router = express.Router();
+const urlCodeController = require('../controllers/urlController')
+
+
+
+
+module.exports= router;
