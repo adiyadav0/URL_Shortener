3a9dcb2b731af5129d884b561ac63ad2513d294f
aditisah <sditi.sah1@gmail.com>
2022-07-18 14:05:58 +0530

    validation added

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,33 +1,47 @@
-const urlModel = require('../models/urlModel');
+const urlModel = require("../models/urlModel");
+const shortid = require("shortid");
+const validUrl = require("valid-url");
 
-var shortid = require('shortid');
-
-const shortenUrl = async function(req, res){
-    try{
-        const getUrl = req.body;
-        const baseUrl = 'http://localhost:3000/';
-const isUrlShortened = await urlModel.findOne({longUrl: getUrl.longUrl});
-if(isUrlShortened){
-    res.status(400).send({status: false, message: 'Given url already has been shortened!!', shortUrl: isUrlShortened.shortUrl})
-    return
-}
-const code = shortid.generate();
-const shortUrl = baseUrl.concat(code)
-const urlDetail = {
-    longUrl: getUrl.longUrl,
-    shortUrl: shortUrl,
-    urlCode: code
-}
-let createShortUrl = await urlModel.create(urlDetail);
-res.status(201).send({status: true, message: 'Success', data: urlDetail})
-return
+const shortenUrl = async function (req, res) {
+  try {
+    const longUrl = req.body.longUrl;
+    const baseUrl = "http://localhost:3000/";
+    
+    if (Object.keys(longUrl).length === 0) {
+      res.status(400).send({ status: false, message: "Please provide url" });
+      return;
     }
-catch(err){
-res.status(500).send({status: false, message: err.message})
-return
-}
-}
+    if (!validUrl.isUri(longUrl)) {
+      res
+        .status(400)
+        .send({ status: false, message: "Given url is not valid" });
+      return;
+    }
+    const isUrlShortened = await urlModel.findOne({ longUrl: longUrl });
+    if (isUrlShortened) {
+      res
+        .status(400)
+        .send({
+          status: false,
+          message: "Given url already has been shortened!!",
+          shortUrl: isUrlShortened.shortUrl,
+        });
+      return;
+    }
+    const code = shortid.generate();
+    const shortUrl = baseUrl.concat(code);
+    const urlDetail = {
+      longUrl: longUrl,
+      shortUrl: shortUrl,
+      urlCode: code,
+    };
+    let createShortUrl = await urlModel.create(urlDetail);
+    res.status(201).send({ status: true, message: "Success", data: urlDetail });
+    return;
+  } catch (err) {
+    res.status(500).send({ status: false, message: err.message });
+    return;
+  }
+};
 
-
-
-module.exports.shortenUrl = shortenUrl
+module.exports.shortenUrl = shortenUrl;
