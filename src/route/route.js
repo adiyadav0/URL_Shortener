ead8eaa3b7289fcae9366fe8d8c4076ebaf4d527
aditisah <sditi.sah1@gmail.com>
2022-07-20 17:55:25 +0530

    caching implemented

--- src/route/route.js
+++ src/route/route.js
@@ -4,7 +4,7 @@ const urlController = require("../controllers/urlController");
 
 router.post("/url/shorten", urlController.shortenUrl);
 router.get("/:urlCode", urlController.getUrl);
-router.all('/**', function(req,res){
+router.all('/*', function(req,res){
     res.status(400).send({status:false,message:"Invaild params"})
 })
 module.exports = router;
